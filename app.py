fdb35036782af0b9a2db501382ffcd5000edd556
Tim O'Donnell <timodonnell@gmail.com>
2017-10-24 23:46:42 -0400

    Add iedb API entrypoints. Closes #9

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import socket
+
 import pandas
 from flask import Flask, render_template, request, flash, redirect, url_for
 
@@ -10,7 +12,7 @@ from mhctools import MHCflurry
 from Bio import SeqIO
 
 app = Flask(__name__)
-app.secret_key = "this is mhcflurry secret key"
+app.secret_key = "this is mhcflurry secret key" + str(socket.gethostname())
 _predictor = Class1AffinityPredictor.load()
 
 from io import StringIO
@@ -30,7 +32,7 @@ def check_peptide_validity(peptides, min_length, max_length):
     return pandas.Series(peptides).str.match(valid_peptide_regex).values
 
 
-def predict_peptide(peptides, alleles):
+def predict_peptides(peptides, alleles):
     if not peptides:
         return None
 
@@ -108,15 +110,13 @@ def get_results():
             raw_result_df = predict_fasta(
                 form_peptides, alleles=alleles)
         else:
-            raw_result_df = predict_peptide(
+            raw_result_df = predict_peptides(
                 form_peptides.upper().split(), alleles=alleles)
     except Exception as e:
         flash(str(e))
         return redirect(url_for('main'))
 
     if raw_result_df is None or len(raw_result_df) == 0:
-        result_df = pandas.DataFrame(
-            columns=["peptide", "length", "tightest_affinity"])
         flash("Your query resulted in no predictions.")
         return redirect(url_for('main'))
     else:
@@ -138,6 +138,41 @@ def get_results():
         software_note=_SOFTWARE_VERSIONS_STRING,
         mhcflurry_version=mhcflurry.__version__,
         result=result_df)
+
+@app.route('/api-predict', methods=["POST"])
+def iedb_api_predict():
+    print(request.form)
+    alleles = [
+        str(allele) for allele in request.form['allele'].split() if allele
+    ]
+    form_peptides = request.form['peptide'].strip()[:10000000]
+    peptides = [
+        peptide.strip()
+        for peptide in form_peptides.upper().split(",")
+    ]
+    if not peptides:
+        return "ERROR: no peptide given"
+    if not alleles:
+        return "ERROR: no alleles given"
+
+    try:
+        result_df = predict_peptides(peptides, alleles=alleles)
+        result_df = result_df[["peptide", "length", "allele", "affinity"]]
+    except ValueError as e:
+        return "ERROR: %s" % e.args[0]
+    return result_df.to_csv(sep="\t", index=False, float_format="%0.4f")
+
+
+@app.route('/alleles', methods=["GET"])
+def iedb_api_supported_alleles():
+    peptide_lengths = ",".join(str(x) for x in range(
+        _predictor.supported_peptide_lengths[0],
+        _predictor.supported_peptide_lengths[1] + 1))
+    strings = [
+        "%s\t%s" % (allele, peptide_lengths)
+        for allele in _predictor.supported_alleles
+    ]
+    return "\n".join(strings)
 
 
 app.debug = True
